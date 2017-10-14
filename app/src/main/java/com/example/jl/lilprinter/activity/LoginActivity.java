d5af246ebbc6dd2370dec524b3658d33a16ffaa9
Jason Chan <jchan72@gatech.edu>
2017-10-14 01:41:22 -0400

    switch from map to login

--- app/src/main/java/com/example/jl/lilprinter/activity/LoginActivity.java
+++ app/src/main/java/com/example/jl/lilprinter/activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.jl.lilprinter.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,7 +12,7 @@ import com.example.jl.lilprinter.R;
 
 public class LoginActivity extends AppCompatActivity {
     private boolean isLoggedIn = false;
-
+    public static final String MESSAGE = "ISLOGGEDIN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +30,17 @@ public class LoginActivity extends AppCompatActivity {
                     username = usernameEdit.getText().toString();
                 }
                 if (username.length() > 11) {
-                    if (username.substring(username.length() - 12, username.length()).equals("@gatech.edu")) {
+                    if (username.substring(username.length() - 11, username.length()).equals("@gatech.edu")) {
                         isLoggedIn = true;
+                        Intent intent = new Intent(v.getContext(), MapActivity.class);
+                        intent.putExtra(MESSAGE, isLoggedIn);
+                        startActivity(intent);
                     }
+                } else {
+                    Toast toast = Toast.makeText(v.getContext(),
+                            "username ", Toast.LENGTH_SHORT);
+                    toast.show();
                 }
-                Toast toast = Toast.makeText(v.getContext(),
-                        "That is not a valid Georgia Tech email", Toast.LENGTH_SHORT);
-                toast.show();
-
             }
         });
 
