83f174f0cd861d241ec2c8f4c78d96e0e4c9800c
guess-hwu <shany198@gmail.com>
2017-10-14 00:02:39 -0400

    WrekAdmin update

---
+++ app/src/main/java/com/example/jl/lilprinter/model/WrekAdmin.java
@@ -0,0 +1,17 @@
+package com.example.jl.lilprinter.model;
+
+/**
+ * Created by Shannon on 10/13/2017.
+ */
+
+public class WrekAdmin extends GTuser {
+
+    public void addPrinter() {
+        Printer newPrinter = new Printer();
+    }
+
+    public void deletePrinter() {
+        //HOOOOOOOOOOOW
+    }
+
+}
