eb758971e7177543791f0505a1dad970e584a556
Jason Chan <jchan72@gatech.edu>
2017-10-14 00:56:46 -0400

    back end for login activity

--- app/src/main/java/com/example/jl/lilprinter/activity/LoginActivity.java
+++ app/src/main/java/com/example/jl/lilprinter/activity/LoginActivity.java
@@ -1,15 +1,44 @@
 package com.example.jl.lilprinter.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.jl.lilprinter.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private boolean isLoggedIn = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.btn_logLogin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText usernameEdit = (EditText) findViewById(R.id.editTxt_logUsername);
+
+                String username = "";
+                if (usernameEdit.getText() != null) {
+                    username = usernameEdit.getText().toString();
+                }
+                if (username.length() > 11) {
+                    if (username.substring(username.length() - 12, username.length()).equals("@gatech.edu")) {
+                        isLoggedIn = true;
+                    }
+                }
+                Toast toast = Toast.makeText(v.getContext(),
+                        "That is not a valid Georgia Tech email", Toast.LENGTH_SHORT);
+                toast.show();
+
+            }
+        });
+
     }
 }
