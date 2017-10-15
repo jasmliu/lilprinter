751f8a2aa312da1ca2038bacfd1599b4475143d2
j <jasmineliu@gatech.edu>
2017-10-14 20:48:07 -0400

    recyclerview functional

--- app/src/main/java/com/example/jl/lilprinter/activity/ListActivity.java
+++ app/src/main/java/com/example/jl/lilprinter/activity/ListActivity.java
@@ -2,34 +2,25 @@ package com.example.jl.lilprinter.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.jl.lilprinter.R;
-import com.example.jl.lilprinter.adapter.PrinterViewHolder;
-import com.example.jl.lilprinter.data.PrinterAdapter;
+import com.example.jl.lilprinter.adapter.PrinterRecyclerViewAdapter;
 import com.example.jl.lilprinter.model.Printer;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.R.attr.value;
-
 public class ListActivity extends AppCompatActivity {
-    private static final String TAG = "ListActivityERROR";
+    private static final String TAG = "ListActivityError";
 
     private DatabaseReference mDatabase;
     private DatabaseReference printerCloudEndPoint;
@@ -38,8 +29,8 @@ public class ListActivity extends AppCompatActivity {
 
     private ChildEventListener mChildEventListener;
 
-    private List<String> astrings;
-
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,34 +39,19 @@ public class ListActivity extends AppCompatActivity {
 
         mDatabase =  FirebaseDatabase.getInstance().getReference();
         printerCloudEndPoint = mDatabase.child("printers");
+        mRecyclerView = findViewById(R.id.printer_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        //list of printers pulled from database
         mPrinters = new ArrayList<>();
+        mAdapter = new PrinterRecyclerViewAdapter(mPrinters);
+        mRecyclerView.setAdapter(mAdapter);
 
-        mPrinters.add(new Printer());
-
-        astrings = new ArrayList<>();
-
-        /*
-        printerCloudEndPoint.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot printerSnapshot : dataSnapshot.getChildren()) {
-                    Printer printer = printerSnapshot.getValue(Printer.class);
-                    mPrinters.add(printer);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.d(TAG, databaseError.getMessage());
-            }
-        });*/
+        DividerItemDecoration mDividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(), LinearLayoutManager.VERTICAL);
+        mRecyclerView.addItemDecoration(mDividerItemDecoration);
 
         dataRead();
         printerCloudEndPoint.addChildEventListener(mChildEventListener);
-        Log.v(TAG, "CHOCOLATE" + mPrinters.get(0));
-        Log.v(TAG, "SIZE" + astrings.size());
     }
 
     private void dataRead() {
@@ -83,10 +59,10 @@ public class ListActivity extends AppCompatActivity {
             mChildEventListener = new ChildEventListener() {
                 @Override
                 public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                    Printer value = dataSnapshot.getValue(Printer.class);
-                    mPrinters.add(value);
-                    astrings.add(value.getLocation());
-                    Log.v(TAG, astrings.get(0));
+                    Printer printer = dataSnapshot.getValue(Printer.class);
+                    mPrinters.add(printer);
+                    mAdapter.notifyDataSetChanged();
+                    Log.v(TAG, printer.getLocation());
                 }
 
                 @Override
@@ -94,7 +70,8 @@ public class ListActivity extends AppCompatActivity {
                     for (DataSnapshot printerSnapshot : dataSnapshot.getChildren()) {
                         Printer printer = printerSnapshot.getValue(Printer.class);
                         mPrinters.add(printer);
-                        astrings.add(printer.getLocation());
+                        mAdapter.notifyDataSetChanged();
+                        Log.v(TAG, printer.getLocation());
                     }
                 }
 
@@ -113,15 +90,6 @@ public class ListActivity extends AppCompatActivity {
 
                 }
             };
-            Log.v(TAG, "CHOCOLATE" + Integer.toString(mPrinters.size()));
         }
     }
-
 }
-
-//initial printer data add; disregard
-/**
- mPrinterAdapter = new PrinterAdapter();
- for (Printer p : new Printers().getPrinters()) {
- mPrinterAdapter.writePrinter(p);
- }**/
